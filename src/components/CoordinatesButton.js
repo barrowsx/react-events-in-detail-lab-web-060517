b18f544c4991ba7afb1565870511ede050a58ea4
barrowsx <wwboyer14@mail.strakejesuit.org>
2017-08-08 13:58:02 -0400

    𝔠𝔬𝔪𝔪𝔦𝔱𝔱𝔢𝔡 𝔲𝔰𝔦𝔫𝔤 𝔞𝔯𝔠𝔞𝔫𝔢 𝔪𝔞𝔤𝔦𝔠𝔨𝔰 𝔩𝔬𝔰𝔱 𝔱𝔬 𝔱𝔥𝔦𝔰 𝔪𝔬𝔯𝔱𝔞𝔩 𝔠𝔬𝔦𝔩

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,20 @@
-// Code CoordinatesButton Component Here
+import React from 'react'
+
+export default class CoordinatesButton extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  findXY = (event) => {
+    let x = event.clientX
+    let y = event.clientY
+    let arr = [x,y]
+    this.props.onReceiveCoordinates(arr)
+  }
+
+  render(){
+    return (
+      <button onClick={this.findXY}></button>
+    )
+  }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,16 @@
-// Code DelayedButton Component Here
+import React from 'react'
+
+export default class DelayedButton extends React.Component{
+  delay = (event) => {
+    setTimeout(() => {
+      this.props.onDelayedClick(event)
+      event.persist()
+    }, this.props.delay)
+  }
+
+  render(){
+    return (
+      <button onClick={this.delay}></button>
+    )
+  }
+}
